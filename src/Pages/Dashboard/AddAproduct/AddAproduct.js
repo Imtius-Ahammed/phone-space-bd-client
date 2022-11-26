1f7f203e2aedb55756604822d87847b7aacf36d2
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-26 18:03:08 +0600

    add product on db

--- src/Pages/Dashboard/AddAproduct/AddAproduct.js
+++ src/Pages/Dashboard/AddAproduct/AddAproduct.js
@@ -3,8 +3,42 @@ import toast, { Toaster } from 'react-hot-toast';
 const notify = () => toast.success(" Successfully Added Services");
 
 const AddAproduct = () => {
+  const handleAddProduct= (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const service_id = form.service_id.value;
+    const title = form.title.value;
+    const img = form.img.value;
+    const price = form.price.value;
+    const description = form.description.value;
+
+    const newProduct = {
+      service_id,
+      title,
+      img,
+      price,
+      description,
+    };
+    fetch("http://localhost:5000/addproduct", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${localStorage.getItem('accessToken')}`,
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("Successfully added Product");
+          form.reset();
+        }
+      })
+      .catch((er) => console.error(er));
+  };
   return (
-     <form  className="hero">
+     <form onSubmit={handleAddProduct}  className="hero">
       <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Add A product now!</h1>
